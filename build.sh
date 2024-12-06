83c544e39867d8e8fab4d27867fae9284f7ff63c
geeetech.inc@gmail.com <geeetech.inc@gmail.com>
2024-12-06 14:01:56 +0100

    ''

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ export DEBUG=False
 # Convert static asset files
 python manage.py collectstatic --no-input
 
-# Apply any outstanding database migrations
+#Apply any outstanding database migrations
 python manage.py migrate 
 python manage.py loaddata admin-data.json
 python manage.py loaddata custom_permissions.json
