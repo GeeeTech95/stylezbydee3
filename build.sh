f525b297f52d888c3605bd51b9b0d56fd7d30fd1
geeetech.inc@gmail.com <geeetech.inc@gmail.com>
2024-12-06 10:57:39 +0100

    ''

--- build.sh
+++ build.sh
@@ -15,10 +15,10 @@ python manage.py migrate
 python manage.py loaddata admin-data.json
 python manage.py loaddata custom_permissions.json
 
-export DJANGO_SUPERUSER_EMAIL=admin5@stylezbydee.com
+export DJANGO_SUPERUSER_EMAIL=admin1@stylezbydee.com
 export DJANGO_SUPERUSER_PASSWORD=VlopGoikB8 
 
-#python manage.py createsuperuser --no-input
+python manage.py createsuperuser --no-input
 
 
 
